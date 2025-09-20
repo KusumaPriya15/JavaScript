deb3550984a4959c65ef05157259ad4ea2f5e0b2
Kusuma Priya <163659756+KusumaPriya15@users.noreply.github.com>
2025-09-20 16:12:56 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treats all js code as newer version
+// alert(4+4) //we are using node js,not browser
+console.log(5+2)
+console.log("joey")
+
+let name="harry"
+let age=14
+let isLogged=false
+
+//number ->2 to power 53
+//bigint
+//string ->""
+//boolean ->true/false
+//null ->standalone value
+//undefined
+//symbol ->uniqueness
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
